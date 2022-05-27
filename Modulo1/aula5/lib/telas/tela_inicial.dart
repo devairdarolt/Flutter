5865c2fb75f713c9d74d04c66a3157e64d6a1970
Devair D. Darolt <devairdarolt@gmail.com>
2022-05-26 23:16:24 -0300

    Finalizado aula 5

--- Modulo1/aula5/lib/telas/tela_inicial.dart
+++ Modulo1/aula5/lib/telas/tela_inicial.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 
-class TelaInicial extends StatelessWidget {
+class TelaInicial extends StatefulWidget {
   const TelaInicial({Key? key}) : super(key: key);
+
+  @override
+  State<TelaInicial> createState() => _TelaInicialState();
+}
+
+class _TelaInicialState extends State<TelaInicial> {
+  //Controller para o TextField
+  TextEditingController controller = TextEditingController();
+
+  String texto = 'Tela Inicial';
 
   void funcaoBotao() {
     print("IconButton Pressed");
@@ -10,30 +20,54 @@ class TelaInicial extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          leading: IconButton(
-            icon: const Icon(Icons.ac_unit_outlined, color: Colors.white),
-            onPressed: funcaoBotao,
-          ),
-          //
-          // leading: Container(
-          //   color: Colors.black,
-          //   child: Icon(
-          //     Icons.ac_unit_sharp,
-          //     color: Colors.white,
-          //   ),
-          // ),
-          //
-          title: const Text("Exemplo AppBar"),
-          centerTitle: true,
-          backgroundColor: const Color.fromARGB(255, 0, 92, 48),
+      backgroundColor: Color.fromARGB(255, 227, 226, 226),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.ac_unit_outlined, color: Colors.white),
+          onPressed: funcaoBotao,
         ),
-        body: Container(
-          color: Color.fromARGB(255, 0, 183, 207),
-          margin: const EdgeInsets.symmetric(vertical: 8),
-          height: 100,
-          padding: const EdgeInsets.all(8),
-        ));
+        title: const Text("Exemplo AppBar"),
+        centerTitle: true,
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
+      ),
+      body: Center(
+        // child: ElevatedButton(
+        //   child: const Text("Clique aqui"),
+        //   onPressed: funcaoBotao,
+        // ),
+        child: Container(
+          margin: const EdgeInsets.all(30),
+          child: TextField(
+            controller: controller,
+            onSubmitted:
+                textFildSubmited, // (x) => print(x), ou (x) => textFildSubmited(x), ou (x){print(x);} // VARIAS FORMAS DE FAZER
+            decoration: InputDecoration(
+              labelText: "Nome",
+              hintText: "Nome Sobrenome ",
+              prefixIcon:
+                  Icon(Icons.apple, color: Color.fromARGB(255, 42, 41, 41)),
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: funcaoBotaoModoAviao,
+        child: const Icon(Icons.airplanemode_inactive_outlined,
+            color: Color.fromARGB(255, 255, 255, 255)),
+        backgroundColor: Color.fromARGB(255, 0, 0, 0),
+      ),
+      floatingActionButtonLocation:
+          FloatingActionButtonLocation.centerFloat, //Botão flutuante
+    );
+  }
+
+  //Evento do TextField
+  void textFildSubmited(String value) {
+    print("TextField Submited: $value");
+  }
+
+  void funcaoBotaoModoAviao() {
+    print("O valor de TextFild é ${controller.text}");
   }
 }
