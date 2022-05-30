76b7a0032858433f0a54e3f0cc0d35640afbb95c
Devair D. Darolt <devairdarolt@gmail.com>
2022-05-30 14:30:25 -0300

    Modelo para novos projetos inicial

---
+++ Modulo1/aula0/lib/telas/tela_inicial.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class TelaInicial extends StatefulWidget {
+  const TelaInicial({Key? key}) : super(key: key);
+
+  @override
+  State<TelaInicial> createState() => _TelaInicialState();
+}
+
+class _TelaInicialState extends State<TelaInicial> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
