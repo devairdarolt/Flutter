e79e3f356193ab54280afa1e319f6401796ac581
Devair D. Darolt <devairdarolt@gmail.com>
2022-05-30 14:30:55 -0300

    Inicio da aula 8

---
+++ Modulo1/aula8/lib/app.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+import 'telas/tela_inicial.dart';
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+      ),
+      home: TelaInicial(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
