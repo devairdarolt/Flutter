746aca6d88711dcc3c44651c7f060c3ca0f3173b
Devair D. Darolt <devair.darolt@gesplan.com.br>
2022-06-26 18:02:47 -0300

    Alterações

--- Modulo1/Projeto1/android/app/src/main/kotlin/com/example/aula4/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.aula4
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- Modulo1/Projeto1/lib/models/produto.dart
+++
@@ -1,4 +0,0 @@
-class Produto{
-  String nome;
-  String descricao;
-}
